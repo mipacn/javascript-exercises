514bf60102523e4fc02b302355afad6a9a8a8647
evorty <138242399+mipacn@users.noreply.github.com>
2024-12-04 22:05:56 +0100

    innj

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newstring = string.replace(/[.,! ]/g, "").toLowerCase()
+    let a = newstring.split("").reverse().join("")
+    return newstring == newstring.split("").reverse().join("")
 };
 
 // Do not edit below this line
