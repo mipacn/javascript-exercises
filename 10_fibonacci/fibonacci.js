245d4084646046646917a87c051235638ecfa8de
evorty <138242399+mipacn@users.noreply.github.com>
2024-12-05 07:24:33 +0100

    finally brotha)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    let extotal = 0
+    let total = 1
+    let extra
 
+    if (num < 0) return "OOPS"
+    else if (num == 0) return 0
+    else if (num == 1) return total
+    else {
+        for (let i = 2; i <= num; i++){
+            extra = total
+            total += extotal
+            extotal = extra 
+        }
+        return total
+    }
 };
 
 // Do not edit below this line
