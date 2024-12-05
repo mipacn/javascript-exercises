2834942ff33df24d54696bfcee7e20e42b662717
evorty <138242399+mipacn@users.noreply.github.com>
2024-12-05 16:20:59 +0100

    exercises finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = array[0]
+    for (let i = 0; i < array.length; i++){
+        if (array[i].yearOfDeath == undefined){
+            delete array[i]
+        }
+        else{
+        let current = array[i]
+        if ((current.yearOfDeath - current.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+            oldest = current
+            return oldest
+        }
+        }
+    
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
