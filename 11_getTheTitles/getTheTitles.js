c53cd5ce803cb134526cfec6eaf6db4f8f6a2de7
evorty <138242399+mipacn@users.noreply.github.com>
2024-12-05 07:48:15 +0100

    i

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newarr = []
+    for (let i = 0; i < array.length; i ++){
+        newarr += (array[i].title + ",")
+    }
+    let newnewarr = newarr.split(",")
+    delete newnewarr[-1]
+    return newnewarr
 };
 
 // Do not edit below this line
